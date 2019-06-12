62e411d407637bb47e2adba45908cea8f9b735ee
Jeanette Mui <jemui@ucsc.edu>
2019-06-12 15:55:40 -0700

    Clouds

---
+++ Assets/VolumetricClouds/Scripts/CameraMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private float fadePerSecond = 2.5f;
+    Renderer rend;
+    Camera cam; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Move the object forward along its z axis 1 unit/second.
+        transform.Translate(0, 0, Time.deltaTime);
+
+        rend = GetComponent<Renderer>();
+       // cam = GetComponent<Camera>();
+        rend.material.shader = Shader.Find("Volumetric Clouds/Raymarch Example With Cam");
+
+        Color color = rend.material.GetColor("_Color");
+        
+        if(color.a >= 0) {
+            color = new Color(color.r, color.g, color.b, color.a - (fadePerSecond * Time.deltaTime));
+            rend.material.SetColor("_Color", color);
+          //  print(color.a);
+        } 
+        // else {
+        //      cam.enabled = false;
+        //      print("toggle");
+        // }
+       // inspector color doesnt change
+
+       
+       // material.color = new Color(color.r, color.g, color.b, color.a - (fadePerSecond * Time.deltaTime));
+       // Debug.Log(material.color.a);
+    }
+}
---
+++ Assets/VolumetricClouds/Scripts/RenderMaterialFullscreen.cs
@@ -0,0 +1,37 @@
+﻿// Taken from Sirenix at http://www.blog.sirenix.net/blog/realtime-volumetric-clouds-in-unity
+
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[RequireComponent(typeof(Camera))]
+public class RenderMaterialFullscreen : MonoBehaviour
+{
+    [SerializeField]
+    private Material material;
+
+    [SerializeField]
+    private int width = 100;
+
+    [SerializeField]
+    private int height = 100;
+
+    private void Awake()
+    {
+        int lowResRenderTarget = Shader.PropertyToID("_LowResRenderTarget");
+
+        CommandBuffer cb = new CommandBuffer();
+
+        cb.GetTemporaryRT(lowResRenderTarget, this.width, this.height, 0, FilterMode.Trilinear, RenderTextureFormat.ARGB32);
+
+        // Blit the low-res texture into itself, to re-draw it with the current material
+        cb.Blit(lowResRenderTarget, lowResRenderTarget, this.material);
+
+        // Blit the low-res texture into the camera's target render texture, effectively rendering it to the entire screen
+        cb.Blit(lowResRenderTarget, BuiltinRenderTextureType.CameraTarget);
+
+        cb.ReleaseTemporaryRT(lowResRenderTarget);
+
+        // Tell the camera to execute our CommandBuffer before the forward opaque pass - that is, just before normal geometry starts rendering
+        this.GetComponent<Camera>().AddCommandBuffer(CameraEvent.BeforeForwardOpaque, cb);
+    }
+}
